00cb0b2feeafe2973ea912472470831bbfb75f97
marta <martaribe153@gmail.com>
2021-07-05 08:49:17 +0200

    cambio

--- pruebaJenkins1/src/pruebaJenkins1/Main.java
+++ pruebaJenkins1/src/pruebaJenkins1/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		System.out.println("sigo probando cambios");
 		System.out.println("sigo probando cambios");
 		System.out.println("sigo probando cambios");
+		System.out.println("sigo probando cambios");
 		
 		
 
